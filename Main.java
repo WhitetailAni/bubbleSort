a36bf01259720d1f83177932ead256c8a12e054d
WhitetailAni <davidmichael2006@gmail.com>
2023-03-14 12:32:09 -0500

    Yay methods. Now it does two arrays.

--- Main.java
+++ Main.java
@@ -1,16 +1,28 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        ArrayList bubblePile = new ArrayList();
-        int pileLength = (int)(Math.random() * 20);
-        pileLength = 500;
-        System.out.println(pileLength);
-        for(int i=0; i<pileLength; i++){
-            bubblePile.add((int)(Math.random()*100));
+        int pile1Length = (int)(Math.random() * 200);
+        ArrayList bubblePile1 = bubbleRand(pile1Length);
+        int pile2Length = (int)(Math.random() * 200);
+        ArrayList bubblePile2 = bubbleRand(pile1Length);
+        System.out.println("1st length is " + bubblePile1);
+        System.out.println("2nd length is " + bubblePile2);
+        bubbleSort(bubblePile1, pile1Length);
+        bubbleSort(bubblePile2, pile2Length);
+        System.out.println();
+        System.out.println("1st sorted array: " + bubblePile1);
+        System.out.println("2nd sorted array: " + bubblePile2);
+    }
+    static ArrayList bubbleRand(int genLength){
+        ArrayList bubbleGen = new ArrayList();
+        for(int i=0; i<genLength; i++){
+            bubbleGen.add((int)(Math.random()*100));
         }
-        System.out.println(bubblePile);
+        return bubbleGen;
+    }
+    static void bubbleSort(ArrayList<Integer> bubblePile, int pileLength){
         int increment = 0;
-        int pileLength2 = pileLength;
+        int pileLength2 = bubblePile.size();
         master:
         for(int i=0; i<pileLength2-1; i++) {
             for(int j=0; j<pileLength2-1; j++) {
@@ -26,6 +38,5 @@ public class Main {
                 }
             }
         }
-        System.out.println("Final sorted array: " + bubblePile);
     }
 }
